78025f5a229f5967ed10e6bab0d9800063288d46
IbrahimImanol <89035983+IbrahimImanol@users.noreply.github.com>
2021-09-25 00:30:09 -0500

    Mini Vector

---
+++ MIniVector.h
@@ -0,0 +1,64 @@
+#pragma once
+template<typename T>
+class MVector {
+public:
+	MVector() {
+		this->sizemin = 0;
+		arr = new T[this->sizemin];
+	}
+	~MVector() {
+		for (int i = 0; i < sizemini(); i++) {
+			delete arr[i];
+		}
+		delete[] arr;
+	}
+	void resize(int size,T obj) {
+		for (int i = 0; i < size; i++)
+		{
+			push_backMini(obj);
+		}
+	}
+	void push_backMini(T obj) {
+		T* aux = new T[sizemin + 1];
+		for (int i = 0; i < sizemin; i++) {
+			aux[i] = arr[i];
+		}
+		aux[sizemin] = obj;
+		delete arr;
+		arr = aux;
+		++sizemin;
+		aux = nullptr;
+		delete aux;
+	}
+	int	sizemini() {
+		return sizemin;
+	}
+	T at(int i) {
+		return arr[i];
+	}
+	int begin() {
+		return 0;
+	}
+	void erase(int pos) {
+		if (sizemin > 0 && pos < sizemin && pos >= 0) {
+			T* aux = new T[sizemin - 1];
+			for (int i = 0; i < sizemin - 1; i++) {
+				if (i < pos) {
+					aux[i] = arr[i];
+				}
+				else {
+					aux[i] = arr[i + 1];
+				}
+			}
+			delete arr;
+			arr = aux;
+			--sizemin;
+			aux = nullptr;
+			delete aux;
+		}
+		else { return; }
+	}
+private:
+	int sizemin;
+	T* arr;
+};
